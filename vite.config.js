0acf7e073a168814f2050b28021eb98363d4bdc1
Sana Hodaie <sana.hodaieo1384@gmail.com>
2026-06-10 02:57:25 -0700

    change context of vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/coffeeshopwebApp/',
   plugins: [react()],
+  // base را کاملاً حذف کنید یا خالی بگذارید
+  base: '/',
 })
